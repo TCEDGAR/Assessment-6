6a8322b4913765ffb69898abb1cdd9d2141479eb
TCEDGAR <tcedgarroman@gmail.com>
2025-08-27 19:19:31 +0530

    Add files via upload

---
+++ DebtApi/Models/Dept.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeptApi.Models
+{
+    public class Dept
+    {
+        [Key]
+        public int DeptId { get; set; }
+        public string DeptName { get; set; } = string.Empty;
+        public int NumOfEmployees { get; set; }
+        public int ManagerId { get; set; }
+        [ForeignKey("ManagerId")]
+        public Manager? Manager { get; set; }
+
+    }
+}
---
+++ DebtApi/Models/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeptApi.Models
+{
+    public class Manager
+    {
+        [Key]
+        public int ManagerId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public ICollection<Dept>? Departments { get; set; }
+
+    }
+}
