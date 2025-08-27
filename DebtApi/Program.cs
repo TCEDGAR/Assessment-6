ce90649285e330c2cfe5639df40493844975a966
TCEDGAR <tcedgarroman@gmail.com>
2025-08-27 19:10:37 +0530

    Add files via upload

---
+++ DebtApi/Program.cs
@@ -0,0 +1,22 @@
+using DeptApi.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DeptDb")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
